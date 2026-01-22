ae052778b878fc61b582be7262725755dbb29330
Mohamed Rabie Becha <150073810+redknight456@users.noreply.github.com>
2026-01-22 19:20:23 +0100

    Set main branch to current state

--- src/main/java/com/skillsync/mapper/CourseMapper.java
+++ src/main/java/com/skillsync/mapper/CourseMapper.java
@@ -4,10 +4,10 @@ import com.skillsync.dto.CourseDto;
 import com.skillsync.entity.Course;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CourseMapper {
-    @Mapping(target = "enrollments", ignore = true)
     CourseDto toDto(Course course);
 
     @Mapping(target = "enrollments", ignore = true)
